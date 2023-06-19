3262a048f0f8e12591482438d6698111b00a2c60
miniHost <miniHost@DESKTOP>
2023-06-19 22:28:53 +0800

    1. modify to .net 7 2. add blazor hybrid

---
+++ RemoteClientBlazor/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using RemoteClient.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RemoteClientBlazor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var jsonPath = "AppSettings.json";
+            if (!File.Exists(jsonPath))
+            {
+                throw new Exception("AppSettings.json missing!");
+            }
+            string json = File.ReadAllText(jsonPath) ?? string.Empty;
+            var options = new JsonSerializerOptions
+            {
+                ReadCommentHandling = JsonCommentHandling.Skip
+            };
+            AppSettings settings = JsonSerializer.Deserialize<AppSettings>(json, options);
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddMudServices();
+            serviceCollection.AddSingleton<AppSettings>(settings);
+            var provider = serviceCollection.BuildServiceProvider();
+            Resources.Add("services", provider);
+            Ioc.Default.ConfigureServices(provider);
+
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ RemoteClientBlazor/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using RemoteClient.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoteClientBlazor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var folder = System.IO.Path.Combine(Environment.CurrentDirectory, "temp");
+            if (Directory.Exists(folder))
+            {
+                var files = Directory.GetFiles(folder);
+                foreach (var file in files)
+                {
+                    File.Delete(file);
+                }
+            }            
+        }
+    }
+}
